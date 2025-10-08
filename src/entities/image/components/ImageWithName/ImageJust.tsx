d38b7ef26811aa172ee05dbdc55f8c367c6df00b
1ww1s <ctop124@mail.ru>
2025-10-08 03:24:21 +0300

    add. new feature: увеличитель изображения. Добавлена в фулл версию просмотра сертификатов

--- src/entities/image/components/ImageWithName/ImageJust.tsx
+++ src/entities/image/components/ImageWithName/ImageJust.tsx
@@ -8,10 +8,12 @@ interface ImageJustProps {
 
 export const ImageJust: FC<ImageJustProps> = ({image}) => {
 
+    const src = image?.blobUrl ? image?.blobUrl : `${process.env.NEXT_PUBLIC_SERVER_URL_API}${image?.url}`;
+
     return (
         <div className={classes.imageJust}>
             <img 
-                src={image?.blobUrl ? image?.blobUrl : `${process.env.NEXT_PUBLIC_SERVER_URL_API}${image?.url}`} 
+                src={src} 
                 alt={image?.name}
             />
             <span data-name={true} className={classes.name}>{image?.name}</span>
